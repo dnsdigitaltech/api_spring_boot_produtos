e21fcb4dd7beb7f102cb6ed5807ed4bcb3a3deee
Davi Bernardo <dnssites@gmail.com>
2021-11-16 20:28:40 -0300

    Criando a model, o repository e a conecçao do banco mysql de Produtos

---
+++ src/main/java/com/sosapi/crm/sosapi/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.sosapi.crm.sosapi.repository;
+
+import com.sosapi.crm.sosapi.models.Produto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+    
+}
---
+++ src/test/java/com/sosapi/crm/sosapi/SosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sosapi.crm.sosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
