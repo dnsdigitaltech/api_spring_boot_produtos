36f6cc96f6f39d5472fb9c5a340c1fa6c82dd361
Davi Bernardo <dnssites@gmail.com>
2021-11-17 08:20:20 -0300

    Metodo para deletar o dado

--- src/main/java/com/sosapi/crm/sosapi/controller/ProdutoResource.java
+++ src/main/java/com/sosapi/crm/sosapi/controller/ProdutoResource.java
@@ -4,6 +4,7 @@ import com.sosapi.crm.sosapi.repository.ProdutoRepository;
 import com.sosapi.crm.sosapi.models.Produto;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,5 +33,10 @@ public class ProdutoResource {
     @PostMapping("/produto")
     public Produto salvaProduto(@RequestBody Produto produto) {
         return produtoRepository.save(produto);
+    }
+
+    @DeleteMapping("/produto")
+    public void deletaProduto(@RequestBody Produto produto) {
+        produtoRepository.delete(produto);
     }
 }
